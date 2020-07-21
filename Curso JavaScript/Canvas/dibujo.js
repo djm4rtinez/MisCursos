0865d1f80828c364e686da2c94a7cef8eb3d3831
David Julian Martinez Luengas <djmartinez@Bancofalabella.com.co>
2020-07-21 08:55:16 -0500

    Canvas con figura en While terminada solo parte inferior izquierda

--- Curso JavaScript/Canvas/dibujo.js
+++ Curso JavaScript/Canvas/dibujo.js
@@ -2,9 +2,33 @@ var d = document.getElementById("dibujo");
 var lienzo = d.getContext("2d"); //Contexto en el que queremos trabajar
 var lineas = 30;
 var i = 0;
+var color = "#AAF"
+var xi = 0;
+var xf;
+var yi;
+var yf = 300;
 
-dibujar('#AAF',0,0,10,300)
-dibujar('AAF',0,10,20,300)
+while(i < lineas)
+{
+    console.log("valor de i, iniciando while: " + i);
+    yi = 10 * i
+    xf = 10 * (i +1);
+    console.log("valor de i despues de xf: " + i);
+    dibujar(color,xi,yi,xf,yf)
+    //dibujar('#AAF',0,10,20,300)
+    i=i+1;
+    console.log("valor de i, terminando while: " + i);
+}
+dibujar('#AAF',1,1,1,299);
+dibujar('#AAF',1,299,299,299);
+
+for(i=0; i<lineas; i++)
+{
+    
+}
+
+
+
 
 function dibujar(color, xinicial, yinicial, xfinal, yfinal)
 {
