d6cc92b13aeca866059b9d09c141e4d35f3c20e7
David Julian Martinez Luengas <djmartinez@Bancofalabella.com.co>
2020-08-12 23:51:54 -0500

    Ultimos cambios antes de subir a GitHub

--- Curso JavaScript/Canvas/dibujo.js
+++ Curso JavaScript/Canvas/dibujo.js
@@ -1,41 +1,11 @@
+var texto = document.getElementById("txt_lineas");
+var boton = document.getElementById("btn_validar");
+boton.addEventListener("click", dibujarPorClic)
+
 var d = document.getElementById("dibujo");
 var lienzo = d.getContext("2d"); //Contexto en el que queremos trabajar
-var lineas = 30;
-var i = 0;
-var color = "#AAF"
-var xi = 0;
-var xf;
-var yi;
-var yf = 300;
-
-while(i < lineas)
-{
-    //console.log("valor de i, iniciando while: " + i);
-    yi = 10 * i
-    xf = 10 * (i +1);
-    //console.log("valor de i despues de xf: " + i);
-    dibujar(color,xi,yi,xf,yf)
-    //dibujar('#AAF',0,10,20,300)
-    i=i+1;
-    //console.log("valor de i, terminando while: " + i);
-}
-dibujar('#AAF',1,1,1,299);
-dibujar('#AAF',1,299,299,299);
-
-
-for(i=0; i<lineas; i++)
-{
-    xi=300;
-    yf=300;
-
-    yi = 10 * i
-    xf = xi-(10 * (i+1));
-    console.log("xf: " + xf);
-    dibujar(color,xi,yi,xf,yf)
-}
-dibujar('#AAF',299,1,299,299);
-dibujar('#AAF',299,299,1,299);
-
+var ancho = d.width;
+texto.dis
 
 
 function dibujar(color, xinicial, yinicial, xfinal, yfinal)
@@ -46,4 +16,46 @@ function dibujar(color, xinicial, yinicial, xfinal, yfinal)
     lienzo.lineTo(xfinal,yfinal); //Función para trazar la linea
     lienzo.stroke(); //dibuja la linea con el estulo que se definio
     lienzo.closePath();
+}
+
+
+function dibujarPorClic()
+{
+    var lineas = parseInt(texto.value);
+    var i = 0;
+    var espacio = ancho/lineas;
+    console.log("Datos ancho, lineas y espacio: " + ancho + ", " + lineas + ", " + espacio);
+    var color = "#AAF"
+    var xi = 0;
+    var xf;
+    var yi;
+    var yf = 300;
+
+    while(i < lineas)
+    {
+        //console.log("valor de i, iniciando while: " + i);
+        yi = espacio * i
+        xf = espacio * (i +1);
+        //console.log("valor de i despues de xf: " + i);
+        dibujar(color,xi,yi,xf,yf)
+        //dibujar('#AAF',0,10,20,300)
+        i=i+1;
+        //console.log("valor de i, terminando while: " + i);
+    }
+    dibujar('#AAF',1,1,1,299);
+    dibujar('#AAF',1,299,299,299);
+
+    for(i=0; i<lineas; i++)
+    {
+        xi=300;
+        yf=300;
+
+        yi = espacio * i
+        xf = xi-(espacio * (i+1));
+        //console.log("xf: " + xf);
+        dibujar(color,xi,yi,xf,yf)
+    }
+    dibujar('#AAF',299,1,299,299);
+    dibujar('#AAF',299,299,1,299);
+
 }
